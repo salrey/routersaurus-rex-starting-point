e3e11dffa16593f4aed0ac49283c6ab73b6fcdd8
salrey <tekdehn@gmail.com>
2021-11-30 12:12:02 -0500

    Toggle Components Manually

--- src/App.js
+++ src/App.js
@@ -1,34 +1,58 @@
 import { Component } from "react";
-import Dinosaurs from "./components/Dinosaurs";
+// import Dinosaurs from "./components/Dinosaurs";
+import { Link, Route } from "react-router-dom";
 import "./App.css";
+import Herbivores from "./components/Herbivores";
+import Carnivores from "./components/Carnivores";
 
 class App extends Component {
   constructor() {
     super();
     this.state = {
-      carnivoresShown: true,
-      herbivoresShown: true,
+      carnivoresShown: false,
+      herbivoresShown: false,
     };
   }
 
+  toggleDinoTypes = (event) => {
+    // console.log(!this.state[event.target.textContent.toLowerCase() + "Shown"])
+    this.setState({
+      [event.target.textContent.toLowerCase() + "Shown"]: !this.state[event.target.textContent.toLowerCase() + "Shown"]
+    })
+  }
+
+  showAll = () => {
+    this.setState({
+      carnivoresShown: true,
+      herbivoresShown: true
+    })
+  }
+
   render() {
+    const { carnivoresShown, herbivoresShown } = this.state;
+
     return (
       <div className="App">
         <h1>Dinosaurs! They're cool! I GUESS.</h1>
         <header>
           <ul>
             <li>
-              <a href="#">Herbivores</a>
+              <a onClick={this.toggleDinoTypes} href="#">Herbivores</a>
             </li>
             <li>
-              <a href="#">Carnivores</a>
+              <a onClick={this.toggleDinoTypes} href="#">Carnivores</a>
             </li>
             <li>
-              <a href="#">All</a>
+              <a onClick={this.showAll} href="#">All</a>
             </li>
           </ul>
         </header>
-        <Dinosaurs />
+        {/* <Dinosaurs /> */}
+        {herbivoresShown ? <Herbivores /> : null}
+        {carnivoresShown && <Carnivores />}
+
+        {/* {conditionals use case} */}
+        {/* {this.state.loggedIn ? <Profile /> : <SignUp />} */}
       </div>
     );
   }
