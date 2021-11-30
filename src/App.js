73b640854790ed1d46e15fd1e962aa59041e3719
salrey <tekdehn@gmail.com>
2021-11-30 12:30:34 -0500

    Update links

--- src/App.js
+++ src/App.js
@@ -6,30 +6,30 @@ import Herbivores from "./components/Herbivores";
 import Carnivores from "./components/Carnivores";
 
 class App extends Component {
-  constructor() {
-    super();
-    this.state = {
-      carnivoresShown: false,
-      herbivoresShown: false,
-    };
-  }
+  // constructor() {
+  //   super();
+  //   this.state = {
+  //     carnivoresShown: false,
+  //     herbivoresShown: false,
+  //   };
+  // }
 
-  toggleDinoTypes = (event) => {
-    // console.log(!this.state[event.target.textContent.toLowerCase() + "Shown"])
-    this.setState({
-      [event.target.textContent.toLowerCase() + "Shown"]: !this.state[event.target.textContent.toLowerCase() + "Shown"]
-    })
-  }
+  // toggleDinoTypes = (event) => {
+  //   // console.log(!this.state[event.target.textContent.toLowerCase() + "Shown"])
+  //   this.setState({
+  //     [event.target.textContent.toLowerCase() + "Shown"]: !this.state[event.target.textContent.toLowerCase() + "Shown"]
+  //   })
+  // }
 
-  showAll = () => {
-    this.setState({
-      carnivoresShown: true,
-      herbivoresShown: true
-    })
-  }
+  // showAll = () => {
+  //   this.setState({
+  //     carnivoresShown: true,
+  //     herbivoresShown: true
+  //   })
+  // }
 
   render() {
-    const { carnivoresShown, herbivoresShown } = this.state;
+    // const { carnivoresShown, herbivoresShown } = this.state;
 
     return (
       <div className="App">
@@ -37,26 +37,29 @@ class App extends Component {
         <header>
           <ul>
             <li>
-              <a onClick={this.toggleDinoTypes} href="#">Herbivores</a>
+              <Link onClick={this.toggleDinoTypes} to="/herbivores">Herbivores</Link>
             </li>
             <li>
-              <a onClick={this.toggleDinoTypes} href="#">Carnivores</a>
+              <Link onClick={this.toggleDinoTypes} to="/carnivores">Carnivores</Link>
             </li>
             <li>
-              <a onClick={this.showAll} href="#">All</a>
+              <Link onClick={this.showAll} to="/">All</Link>
             </li>
           </ul>
         </header>
         {/* Modern way of using React Networking/react-router */}
-        {/* NO longer need constructor */}
+        {/* NO longer need constructor and methods */}
         <Routes >
           <Route exact path="/" element={<Dinosaurs />} />
           <Route path="/herbivores" element={<Herbivores />} />
           <Route path="/carnivores" element={<Carnivores />} />
         </Routes>
         {/* Old way of using React Networking/react-router */}
-        {/* <Route path="/herbivores" component={<Herbivores />} /> */}
-
+        {/* <Switch >
+          <Route exact path="/" component={<Dinosaurs />} />
+          <Route path="/herbivores" component={<Herbivores />} />
+          <Route path="/carnivores" component={<Carnivores />} />
+        </Switch> */}
 
         {/* Manual Conditional Rendering */}
         {/* {herbivoresShown ? <Herbivores /> : null}
